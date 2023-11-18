448baf055a9252bb9ef76632a9bfc1ade69814ca
Vasily Soldatov <vasily.soldatov@gmail.com>
2023-11-18 22:55:35 +0300

    Update v3

--- Lesson_005/Lsn05_Tsk02/Lsn05_Tsk02.cpp
+++ Lesson_005/Lsn05_Tsk02/Lsn05_Tsk02.cpp
@@ -5,11 +5,10 @@
 class Figure
 {
 protected:
-    std::string name;
     std::string sides_name = "Стороны";
     std::string angles_name = "Углы";
+    std::string name;
 public:
-    Figure() {}
     virtual void PrintInfo() {}
 };
 
@@ -22,7 +21,18 @@ protected:
     int angle_value_A;
     int angle_value_B;
     int angle_value_C;
+    std::string name;
 public:
+    Triangle(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+        : side_length_a(in_side_length_a),
+        side_length_b(in_side_length_b),
+        side_length_c(in_side_length_c),
+        angle_value_A(in_angle_value_A),
+        angle_value_B(in_angle_value_B),
+        angle_value_C(in_angle_value_C)
+    {
+        name = "Треугольник";
+    }
     Triangle()
     {
         this->side_length_a = 10;
@@ -32,9 +42,6 @@ public:
         this->angle_value_B = 60;
         this->angle_value_C = 70;
     }
-    void print_info(Triangle* triangle1) {
-        PrintInfo();
-    }
     void PrintInfo() override {
         name = GetName();
         std::cout << name << std::endl;
@@ -43,20 +50,40 @@ public:
         std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
         std::cout << angle_value_B << " " << "C=" << angle_value_C << "\n" << std::endl;
     }
-
     std::string GetName()
     {
-        this->name = "Треугольник:";
-        return name;
+        return name = "Треугольник";;
     }
-
-
 };
 
-class RightTriangle : public Triangle {
+class RightTriangle : public Triangle
+{
 public:
     RightTriangle() {
         this->angle_value_C = 90;
+    }
+public:
+    //    RightTriangle(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+    //            : side_length_a(in_side_length_a),
+    //              side_length_b(in_side_length_b),
+    //              side_length_c(in_side_length_c),
+    //              angle_value_A(in_angle_value_A),
+    //              angle_value_B(in_angle_value_B),
+    //              angle_value_C(in_angle_value_C)
+    //    {
+    //        name = "Прямоугольный треугольник";
+    //    }
+    void PrintInfo() override {
+        name = GetName();
+        std::cout << name << std::endl;
+        std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
+        std::cout << side_length_b << " " << "c=" << side_length_c << std::endl;
+        std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
+        std::cout << angle_value_B << " " << "C=" << angle_value_C << "\n" << std::endl;
+    }
+    std::string GetName()
+    {
+        return name = "Прямоугольный треугольник";
     }
 };
 
@@ -67,6 +94,29 @@ public:
     {
         this->side_length_c = 10;
         this->angle_value_C = 50;
+    }
+public:
+    //    RightTriangle(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+    //            : side_length_a(in_side_length_a),
+    //              side_length_b(in_side_length_b),
+    //              side_length_c(in_side_length_c),
+    //              angle_value_A(in_angle_value_A),
+    //              angle_value_B(in_angle_value_B),
+    //              angle_value_C(in_angle_value_C)
+    //    {
+    //        name = "Равнобедренный треугольник";
+    //    }
+    void PrintInfo() override {
+        name = GetName();
+        std::cout << name << std::endl;
+        std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
+        std::cout << side_length_b << " " << "c=" << side_length_c << std::endl;
+        std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
+        std::cout << angle_value_B << " " << "C=" << angle_value_C << "\n" << std::endl;
+    }
+    std::string GetName()
+    {
+        return name = "Равнобедренный треугольник";
     }
 };
 
@@ -80,6 +130,29 @@ public:
         this->angle_value_A = 60;
         this->angle_value_C = 60;
 
+    }
+public:
+    //    RightTriangle(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+    //            : side_length_a(in_side_length_a),
+    //              side_length_b(in_side_length_b),
+    //              side_length_c(in_side_length_c),
+    //              angle_value_A(in_angle_value_A),
+    //              angle_value_B(in_angle_value_B),
+    //              angle_value_C(in_angle_value_C)
+    //    {
+    //        name = "Равносторонний треугольник";
+    //    }
+    void PrintInfo() override {
+        name = GetName();
+        std::cout << name << std::endl;
+        std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
+        std::cout << side_length_b << " " << "c=" << side_length_c << std::endl;
+        std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
+        std::cout << angle_value_B << " " << "C=" << angle_value_C << "\n" << std::endl;
+    }
+    std::string GetName()
+    {
+        return name = "Равносторонний треугольник";
     }
 };
 
@@ -95,9 +168,17 @@ protected:
     int angle_value_C;
     int angle_value_D;
     std::string name;
-    std::string sides_name = "???????";
-    std::string angles_name = "????";
 public:
+    Quadrangle(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+        : side_length_a(in_side_length_a),
+        side_length_b(in_side_length_b),
+        side_length_c(in_side_length_c),
+        angle_value_A(in_angle_value_A),
+        angle_value_B(in_angle_value_B),
+        angle_value_C(in_angle_value_C)
+    {
+        name = "Четырехугольник";
+    }
     Quadrangle()
     {
         this->side_length_a = 10;
@@ -109,19 +190,33 @@ public:
         this->angle_value_C = 70;
         this->angle_value_D = 80;
     }
-    void PrintInfo(std::string name)
-    {
+    void PrintInfo() override {
+        name = GetName();
         std::cout << name << std::endl;
         std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
         std::cout << side_length_b << " " << "c=" << side_length_c << " " << "d=" << side_length_d << std::endl;
         std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
         std::cout << angle_value_B << " " << "C=" << angle_value_C << " " << "D=" << angle_value_D << "\n" << std::endl;
     }
+    std::string GetName()
+    {
+        return name = "Четырехугольник";
+    }
 };
 
 class Parallelogram : public Quadrangle
 {
 public:
+    //    Parallelogram(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+    //            : side_length_a(in_side_length_a),
+    //              side_length_b(in_side_length_b),
+    //              side_length_c(in_side_length_c),
+    //              angle_value_A(in_angle_value_A),
+    //              angle_value_B(in_angle_value_B),
+    //              angle_value_C(in_angle_value_C)
+    //    {
+    //        name = "Параллелограмм";
+    //    }
     Parallelogram()
     {
         this->side_length_a = 20;
@@ -133,11 +228,33 @@ public:
         this->angle_value_C = 30;
         this->angle_value_D = 40;
     }
+    void PrintInfo() override {
+        name = GetName();
+        std::cout << name << std::endl;
+        std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
+        std::cout << side_length_b << " " << "c=" << side_length_c << " " << "d=" << side_length_d << std::endl;
+        std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
+        std::cout << angle_value_B << " " << "C=" << angle_value_C << " " << "D=" << angle_value_D << "\n" << std::endl;
+    }
+    std::string GetName()
+    {
+        return name = "Параллелограмм";
+    }
 };
 
 class RectangleF : public Quadrangle
 {
 public:
+    //    RectangleF(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+//            : side_length_a(in_side_length_a),
+//              side_length_b(in_side_length_b),
+//              side_length_c(in_side_length_c),
+//              angle_value_A(in_angle_value_A),
+//              angle_value_B(in_angle_value_B),
+//              angle_value_C(in_angle_value_C)
+//    {
+//        name = "Прямоугольник";
+//    }
     RectangleF()
     {
         this->side_length_c = 10;
@@ -147,28 +264,89 @@ public:
         this->angle_value_C = 90;
         this->angle_value_D = 90;
     }
+    void PrintInfo() override {
+        name = GetName();
+        std::cout << name << std::endl;
+        std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
+        std::cout << side_length_b << " " << "c=" << side_length_c << " " << "d=" << side_length_d << std::endl;
+        std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
+        std::cout << angle_value_B << " " << "C=" << angle_value_C << " " << "D=" << angle_value_D << "\n" << std::endl;
+    }
+    std::string GetName()
+    {
+        return name = "Прямоугольник";
+    }
 };
 
 class Romb : public Parallelogram
 {
 public:
+    //    Romb(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+//            : side_length_a(in_side_length_a),
+//              side_length_b(in_side_length_b),
+//              side_length_c(in_side_length_c),
+//              angle_value_A(in_angle_value_A),
+//              angle_value_B(in_angle_value_B),
+//              angle_value_C(in_angle_value_C)
+//    {
+//        name = "Ромб";
+//    }
     Romb()
     {
         this->side_length_a = 30;
         this->side_length_c = 30;
+    }
+    void PrintInfo() override {
+        name = GetName();
+        std::cout << name << std::endl;
+        std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
+        std::cout << side_length_b << " " << "c=" << side_length_c << " " << "d=" << side_length_d << std::endl;
+        std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
+        std::cout << angle_value_B << " " << "C=" << angle_value_C << " " << "D=" << angle_value_D << "\n" << std::endl;
+    }
+    std::string GetName()
+    {
+        return name = "Ромб";
     }
 };
 
 class Square : public RectangleF
 {
 public:
+    //    Square(int in_side_length_a, int in_side_length_b, int in_side_length_c, int in_angle_value_A, int in_angle_value_B, int in_angle_value_C)
+//            : side_length_a(in_side_length_a),
+//              side_length_b(in_side_length_b),
+//              side_length_c(in_side_length_c),
+//              angle_value_A(in_angle_value_A),
+//              angle_value_B(in_angle_value_B),
+//              angle_value_C(in_angle_value_C)
+//    {
+//        name = "Квадрат";
+//    }
     Square()
     {
         this->side_length_a = 20;
         this->side_length_c = 20;
 
     }
+    void PrintInfo() override {
+        name = GetName();
+        std::cout << name << std::endl;
+        std::cout << sides_name << ": " << "a=" << side_length_a << " " << "b=";
+        std::cout << side_length_b << " " << "c=" << side_length_c << " " << "d=" << side_length_d << std::endl;
+        std::cout << angles_name << ": " << "A=" << angle_value_A << " " << "B=";
+        std::cout << angle_value_B << " " << "C=" << angle_value_C << " " << "D=" << angle_value_D << "\n" << std::endl;
+    }
+    std::string GetName()
+    {
+        return name = "Квадрат";
+    }
 };
+
+void print_info(Figure* figure)
+{
+    figure->PrintInfo();
+}
 
 int main(int argc, char** argv)
 {
@@ -177,15 +355,30 @@ int main(int argc, char** argv)
     Triangle triangle1;
     print_info(&triangle1);
 
-    //    Triangle().PrintInfo("???????????:");
-    //    RightTriangle().PrintInfo("????????????? ???????????:");
-    //    IsoscelesTriangle().PrintInfo("?????????????? ???????????:");
-    //    EquilateralTriangle().PrintInfo("?????????????? ???????????:");
-    //    Quadrangle().PrintInfo("???????????????:");
-    //    RectangleF().PrintInfo("?????????????:");
-    //    Square().PrintInfo("???????:");
-    //    Parallelogram().PrintInfo("??????????????:");
-    //    Romb().PrintInfo("????:");
+    RightTriangle RightTriangle1;
+    print_info(&RightTriangle1);
+
+    IsoscelesTriangle IsoscelesTriangle1;
+    print_info(&IsoscelesTriangle1);
+
+    EquilateralTriangle EquilateralTriangle1;
+    print_info(&EquilateralTriangle1);
+
+    Quadrangle Quadrangle1;
+    print_info(&Quadrangle1);
+
+    RectangleF RectangleF1;
+    print_info(&RectangleF1);
+
+    Square Square1;
+    print_info(&Square1);
+
+    Parallelogram Parallelogram1;
+    print_info(&Parallelogram1);
+
+    Romb Romb1;
+    print_info(&Romb1);
+
     Sleep(100000);
-    return 0;
+    return 0;
 }
