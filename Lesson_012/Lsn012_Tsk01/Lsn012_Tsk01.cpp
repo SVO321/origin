e3da2909e00af5d5db82c6353060146f385640df
Vasily Soldatov <vasily.soldatov@gmail.com>
2023-12-14 13:30:22 +0300

    Update v3

--- Lesson_012/Lsn012_Tsk01/Lsn012_Tsk01.cpp
+++ Lesson_012/Lsn012_Tsk01/Lsn012_Tsk01.cpp
@@ -8,33 +8,58 @@ static void Calculation(int* arr, int search_point, int size)
     int count = 0;
     int left = 0; 
     int middle = 0;
-    int checked;
+    int checked = 1;
     int right = size - 1;
-    do  
+    while (checked)
     {
-        checked = 0;
+        if (arr[0] > search_point) {
+            count = size;
+            checked = 0;
+        }
+        if (arr[size - 1] < search_point) {
+            count = 0;
+            checked = 0;
+        }
         middle = (left + right) / 2;
+
         if ((arr[middle]) > search_point) {
             right = middle;
         }
+        
         if ((arr[middle]) < search_point) {
-            left = middle + 1;
-            middle = (left + right) / 2;
-            if ((arr[middle]) > search_point) {
-                count = size - middle;
-                checked = 1;
+            if (arr[left] == arr[middle]) {
+                count = size - 1;
+                checked = 0;
+            }
+            if (arr[middle + 1] <= search_point) {
+                left = middle + 1;
+                middle = (left + right) / 2;
+                if ((arr[middle]) > search_point) {
+                    count = size - middle;
+                    checked = 0;
+                }
+            }
+            else {
+                count = (size - 1) - middle;
+                checked = 0;
             }
         }
+        
         if ((arr[middle]) == search_point) {
-            left = middle + 1;
-            if (left == size) { checked = 1; count = 0; }
-            middle = (left + right) / 2;
-            if ((arr[middle]) > search_point) {
-                count = size - middle;
-                checked = 1;
+            if (arr[left] == arr[middle]) {
+                count = size - 1;
+                checked = 0;
+            }
+            if (arr[middle + 1] == search_point) {
+                count = (size - 1) - (middle + 1);
+                checked = 0;
+            }
+            else {
+                count = (size - 1) - middle;
+                checked = 0;
             }
         }
-    } while (checked == 0);
+    }
     std::cout << "Количество элементов в массиве больших, чем " << search_point << ": " << count;
 }
 
